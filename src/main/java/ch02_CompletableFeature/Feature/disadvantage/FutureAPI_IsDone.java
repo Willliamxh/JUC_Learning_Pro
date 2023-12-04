a79d81c58fa05bf487fb747394d84be9a427f8d5
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-12-04 16:49:28 +0800

    2.2.5.2 isDone()轮询 缺点:轮询的方式会耗费无谓的CPU资源，而且也不见得能及时地得到计算结果. 如果想要异步获取结果,通常都会以轮询的方式去获取结果尽量不要阻塞 利用if(futureTask.isDone())的方式使得FutureTask在结束之后才get(),但是也会消耗cpu 通过sleep降低查询的频率,减少cpu的消耗

---
+++ src/main/java/ch02_CompletableFeature/Feature/disadvantage/FutureAPI_IsDone.java
@@ -0,0 +1,33 @@
+package ch02_CompletableFeature.Feature.disadvantage;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author XuHan
+ * @date 2023/12/4 16:36
+ */
+public class FutureAPI_IsDone {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        FutureTask<String> futureTask = new FutureTask<>(() ->{
+            System.out.println(Thread.currentThread().getName()+"\t...come in");
+            TimeUnit.SECONDS.sleep(5);
+            return "task over";
+        });
+        Thread t1 = new Thread(futureTask,"t1");
+        t1.start();
+        System.out.println(Thread.currentThread().getName()+"/t...忙其他任务了");
+        while(true){
+            if(futureTask.isDone()){
+                System.out.println(futureTask.get());
+                break;
+            }else{
+                //暂停毫秒
+                try { TimeUnit.MILLISECONDS.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+                System.out.println("正在处理中，不要再催了，越催越慢 ，再催熄火");
+            }
+        }
+    }
+}
