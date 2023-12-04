d10e0bb8506cb28faadcd0f168beb6d0e77304ac
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-12-04 17:10:27 +0800

    2.3.2 CompletableFuture核心的四个静态方法 2.3.2.2 supplyAsync有返回值 带Executor executor参数的都是可以使用自定义线程池的 默认是用的forkjoinpool，我们可以用自己定义的线程池

--- src/main/java/ch02_CompletableFeature/c02_CompletableFeature/c01/CF02_supplyAsync.java
+++ src/main/java/ch02_CompletableFeature/c02_CompletableFeature/c01/CF02_supplyAsync.java
@@ -1,13 +1,32 @@
 package ch02_CompletableFeature.c02_CompletableFeature.c01;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 /**
  * @author XuHan
  * @date 2023/12/4 17:01
  */
 public class CF02_supplyAsync {
+    private static ThreadPoolExecutor THREAD_POOL;
+    static {
+        // 默认线程池参数
+        int corePoolSize = Runtime.getRuntime().availableProcessors() * 5;
+        int maximumPoolSize = corePoolSize;
+        int keepAliveTime = 0;
+        TimeUnit minutes = TimeUnit.MILLISECONDS;
+        // 定义一个默认的线程池
+        BlockingQueue<Runnable> sendContentWorkQueue = new LinkedBlockingQueue<>(300);
+        ThreadFactory sendContentThreadFactory = new ThreadFactoryBuilder().setNameFormat("THREAD_POOL-%d").build();
+        THREAD_POOL = new ThreadPoolExecutor(3,
+                3,
+                keepAliveTime,
+                minutes,
+                sendContentWorkQueue,
+                sendContentThreadFactory,
+                new ThreadPoolExecutor.CallerRunsPolicy());
+    }
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
             System.out.println(Thread.currentThread() + "running");
@@ -17,8 +36,9 @@ public class CF02_supplyAsync {
                 throw new RuntimeException(e);
             }
             return "over";
-        });
+        },THREAD_POOL);// 注意这边用了自己定义的线程池
         System.out.println(Thread.currentThread() + "running");
         System.out.println(completableFuture.get());
+        THREAD_POOL.shutdown();
     }
 }
