928bb2e564a534d4eb94d9215880652c41e8ee59
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-12-04 17:45:35 +0800

    2.3.3 CompletableFuture通用演示 功能几乎一样,区别在于编码时是否需要抛出异常 get()方法会在编译期间会做异常的检查,因此需要抛出异常或者做异常处理 join()方法不会在编译期间会做异常的检查法

---
+++ src/main/java/ch02_CompletableFeature/c02_CompletableFeature/c01/CF04_Join_Get.java
@@ -0,0 +1,33 @@
+package ch02_CompletableFeature.c02_CompletableFeature.c01;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author XuHan
+ * @date 2023/12/4 17:40
+ */
+public class CF04_Join_Get {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // 抛出异常
+        // get();
+        // 没有抛出异常
+        join();
+
+
+    }
+
+    private static void join() {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            return "hello 12345";
+        });
+        System.out.println(completableFuture.join());
+    }
+
+    private static void get() throws InterruptedException, ExecutionException {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            return "hello 12345";
+        });
+        System.out.println(completableFuture.get());
+    }
+}
