30c6db51f55a3a9f337d7e7221c013c6102ab775
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-12-04 16:28:20 +0800

    2.2.5 FutureTask 的缺点(为什么会使用CompleteableFuture) 2.2.5.1 get方法的阻塞 get方法过时不候.,超过等待时间没有拿到结果直接抛出异常（java.util.concurrent.TimeoutException） 优点:假如我不愿意等待很长时间，我希望过时不候，可以自动离开 缺点:如果写多了抛出大量异常不优雅,但也可以用。

--- src/main/java/ch02_CompletableFeature/Feature/disadvantage/FutureAPIDemo.java
+++ src/main/java/ch02_CompletableFeature/Feature/disadvantage/FutureAPIDemo.java
@@ -19,8 +19,8 @@ public class FutureAPIDemo {
         Thread t1 = new Thread(futureTask,"t1");
         t1.start();
         System.out.println(Thread.currentThread().getName()+"/t...忙其他任务了");
-
-        System.out.println(futureTask.get()); //get方法等待
+//        System.out.println(futureTask.get()); //get方法等待
+        System.out.println(futureTask.get(3,TimeUnit.SECONDS));//get方法过时不候 抛出timeout异常
     }
 
 }
@@ -28,4 +28,5 @@ public class FutureAPIDemo {
 /**
  *1 get容易导致阻塞，一般建议放在程序后面，一旦调用不见不散，非要等到结果才会离开，不管你是否计算完成，容易程序堵塞。
  *2 假如我不愿意等待很长时间，我希望过时不候，可以自动离开.
+ * 当我们在主线程做其他事情之前调用get方法,那么主线程会被阻塞,主线程会一直等到子线程任务执行完毕,get方法拿到返回值为止,那么这样和单线程没有任何区别甚至更慢
  */
